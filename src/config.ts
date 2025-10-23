d3824c4314ce7cf34f277c96a1781cfe4b63c4ad
Tanmay patil <tanmaypatil3151@gmail.com>
2025-10-23 12:29:24 +0530

    deploy.yml file added

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 export const SITE = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  profile: "https://satnaing.dev/",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://tanmaypatil123.github.io/", // replace this with your deployed domain
+  author: "Tanmay Patil",
+  profile: "https://tanmaypatil123.github.io/",
+  desc: "A blog about programming, technology, and software development.",
+  title: "Tanmay Patil's Blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
@@ -14,7 +14,7 @@ export const SITE = {
   editPost: {
     enabled: true,
     text: "Edit page",
-    url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/Tanmaypatil123/Tanmaypatil123.github.io/edit/main/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
